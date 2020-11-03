448c4d9db87a06d0b49098f83b19ab3796ee76bb
Series Chikanbanjar <series7.sc@gmail.com>
2020-11-03 16:18:44 +0545

    Add all the code part (First commit)

---
+++ utility_tools/get_line.py
@@ -0,0 +1,60 @@
+import numpy as np
+import cv2
+
+def getLine(img):
+    # load model
+    net = cv2.dnn.readNet('line_weight/yolov3_training_final.weights', 'line_weight/yolov3_testing.cfg')
+
+
+    # Get all the trained classes
+    classes = []
+    with open("line_weight/classes.txt", "r") as f:
+        classes = f.read().splitlines()
+
+    font = cv2.FONT_HERSHEY_PLAIN
+    colors = np.random.uniform(0, 255, size=(100, 3))
+    height, width, layers = img.shape
+    size = (width,height)
+    blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0,0,0), swapRB=True, crop=False)
+    net.setInput(blob)
+    output_layers_names = net.getUnconnectedOutLayersNames()
+    layerOutputs = net.forward(output_layers_names)
+
+    boxes = []
+    confidences = []
+    class_ids = []
+
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.2:
+                center_x = int(detection[0]*width)
+                center_y = int(detection[1]*height)
+                w = int(detection[2]*width)
+                h = int(detection[3]*height)
+
+                x = int(center_x - w/2)
+                y = int(center_y - h/2)
+
+                boxes.append([x, y, w, h])
+                confidences.append((float(confidence)))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.2, 0.4)
+    
+    if len(indexes)>0:
+        
+        for i in indexes.flatten():
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            confidence = str(round(confidences[i],2))
+            color = colors[i]
+            lg_h = y + h + 20
+            lg_x = x-10
+            lg_y = y-10
+            lg_w = x + w + 20
+        return(img, lg_x, lg_y, lg_w, lg_h)
+    else:
+        return None
